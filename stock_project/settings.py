b8c494a8755eb249d65713a619a14aad65089e21
dharu-0106 <sfm23555@hcctrichy.ac.in>
2025-03-23 17:26:49 +0530

    origin-update

--- stock_project/settings.py
+++ stock_project/settings.py
@@ -50,12 +50,12 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'stock_project.wsgi.application'
 CORS_ALLOW_ALL_ORIGINS = True 
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000",  
-    "http://127.0.0.1:3000",
-    "https://stocks-analysis-three.vercel.app",
-    "https://stocks-back-end.onrender.com",
-]
+# CORS_ALLOWED_ORIGINS = [
+#     "http://localhost:3000",  
+#     "http://127.0.0.1:3000",
+#     "https://stocks-analysis-three.vercel.app",
+#     "https://stocks-back-end.onrender.com",
+# ]
 
 
 # MongoDB Connection
